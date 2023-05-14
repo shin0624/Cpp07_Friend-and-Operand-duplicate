0fea7160d1096fdd284cd6707c5013bfeb5ea301
bbs <91828379+shin0624@users.noreply.github.com>
2023-05-14 20:25:48 +0900

    power 클래스에 ! 연산자 작성
    ! a 는 a의 kick과 punch가 모두 0이면 true, 아니면 false를 리턴.

---
+++ ch07_ex09.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+class Power {
+	int kick, punch;
+public:
+	Power(int kick = 0, int punch = 0) {
+		this->kick = kick, this->punch = punch;
+	}
+	void show();
+	bool operator !();//!연산자함수 선언. !연산자는 a와 b의 매개변수가 0이면 true, 아니면 false를 리턴
+};
+
+void Power::show() {
+	cout << "kick = " << kick << "," << "punch = " << punch << endl;
+}
+
+bool Power ::operator!() {
+	if (kick == 0 && punch == 0) return true;
+	else return false;
+}
+int main() {
+	Power a(0, 0), b(5, 5);
+	if (!a) cout << "a의 파워가 0이다" << endl;
+	else cout << "a의 파워가 0이 아니다." << endl;
+	if (!b) cout << "b의 파워가 0이다" << endl;
+	else cout << "b의 파워가 0이 아니다." << endl;
+
+
+
+}
